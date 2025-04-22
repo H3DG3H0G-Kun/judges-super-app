36d8491095b844109514cbe55fae277a05172e59
H3DG3H0G-Kun <lasha27101998@gmail.com>
2025-04-22 21:42:20 +0400

    the project compiles

--- src/main/java/com/tournament/management/mappers/RuleConfigMapper.java
+++ src/main/java/com/tournament/management/mappers/RuleConfigMapper.java
@@ -4,15 +4,12 @@ import com.tournament.management.dtos.RuleConfigRequest;
 import com.tournament.management.dtos.RuleConfigResponse;
 import com.tournament.management.entities.RuleConfig;
 import org.mapstruct.Mapper;
-
 import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RuleConfigMapper {
-
     RuleConfig toEntity(RuleConfigRequest request);
-    RuleConfigResponse toResponse(RuleConfig config);
-
-    List<RuleConfig> toEntitiesFromRules(List<RuleConfigRequest> requests);
-    List<RuleConfigResponse> toResponsesFromRules(List<RuleConfig> rules);
+    RuleConfigResponse toResponse(RuleConfig ruleConfig);
+    List<RuleConfig> toEntities(List<RuleConfigRequest> requests);
+    List<RuleConfigResponse> toResponses(List<RuleConfig> ruleConfigs);
 }
